58fd703f6b3422a96c26b719585b93121ab31f3c
Uladzimir Ilchanka <yauhenij@gmail.com>
2025-04-02 21:59:55 +0300

    service has been added

--- src/main/java/org/example/model/Message.java
+++ src/main/java/org/example/model/Message.java
@@ -1,10 +1,12 @@
 package org.example.model;
 
 public enum Message {
-    GREETING("Choose your role: "), ADMIN ("Choose your admin business"), USER ("Choose your user business"), WRONG_INPUT("Wrong input, try again");
+    GREETING("Choose your role: "), ADMIN("Choose your admin business"), USER("Choose your user business"), WRONG_INPUT("Wrong input, try again"),
+    SUCCESSFUL("Success!"), NOT_SUCCESSFUL("Not success");
 
     private final String message;
-    private Message(String message){
+
+    private Message(String message) {
         this.message = message;
     }
 
--- src/main/java/org/example/service/ReservationService.java
+++ src/main/java/org/example/service/ReservationService.java
@@ -1,4 +1,51 @@
 package org.example.service;
 
+import org.example.model.*;
+import org.example.repository.DataStorage;
+
+import java.util.List;
+
 public class ReservationService {
+    private final DataStorage dataStorage;
+
+    private ReservationService() {
+        dataStorage = DataStorage.getInstance();
+    }
+
+    public static ReservationService getInstance() {
+        return ReservationServiceHolder.RESERVATION_SERVICE;
+    }
+
+    public List<Reservation> getAllReservations() {
+        return dataStorage.getAllReservations().values().stream().toList();
+    }
+
+    public List<Reservation> getReservationByUserId(final int userId) {
+        return dataStorage.getAllReservations().values().stream().filter(n -> n.getCustomer().getId() == userId).toList();
+    }
+
+    public void makeReservation(final Customer customer, final int idWorkspace, final String start, final String end) {
+        Workspace workspace = dataStorage.getWorkspace(idWorkspace);
+        if (workspace != null && workspace.isAvailable()) {
+            Reservation reservation = new Reservation(customer, workspace, start, end);
+            dataStorage.addReservation(reservation);
+            workspace.setStatus(ReservationStatus.UNAVAILABLE);
+            System.out.println(Message.SUCCESSFUL);
+        }
+        System.out.println(Message.NOT_SUCCESSFUL);
+    }
+
+    public void cancelReservation(final int idReservation) {
+        Reservation reservation = dataStorage.getReservation(idReservation);
+        if (reservation != null) {
+            reservation.cancel(idReservation);
+            reservation.getWorkspace().setStatus(ReservationStatus.AVAILABLE);
+            System.out.println(Message.SUCCESSFUL);
+        }
+        System.out.println(Message.NOT_SUCCESSFUL);
+    }
+
+    private static class ReservationServiceHolder {
+        private static final ReservationService RESERVATION_SERVICE = new ReservationService();
+    }
 }
